458f77a452cdc3d268d19dfb2f1841ff94d0dd1a
shigemk2 <i.am.shige@gmail.com>
2014-02-08 18:19:18 +0900

    レジスタの値をメモリに書き込む https://bitbucket.org/7shi/ikebin/wiki/pdp11/write-4

---
+++ 6/write-4.s
@@ -0,0 +1,42 @@
+/ write(1, hello, 6);
+mov $1, r0
+sys write
+hello
+6
+
+
+/ r1 = hello;
+/ r0 = 0x4548;
+/ *(uint16_t *)r1 = r0;
+mov $hello, r1
+mov $42510, r0
+mov r0, (r1)
+
+/ write(1, hello, 6);
+mov $1, r0
+sys write
+hello
+6
+
+
+/ r1 = hello;
+/ r2 = 0x4c4c;
+/ *(uint16_t *)(r1 + 2) = r2;
+mov $hello, r1
+mov $46114, r2
+mov r2, 2(r1)
+
+/ write(1, hello, 6);
+mov $1, r0
+sys write
+hello
+6
+
+
+/ exit(0);
+mov $0, r0
+sys exit
+
+
+.data
+hello: <hello\n>
