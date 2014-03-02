12dc038a1e29620b7d1ffd0f29604b39602c89b5
shigemk2 <i.am.shige@gmail.com>
2014-03-02 18:03:09 +0900

    6桁のコードからモードを判定する

--- pdf11-10/10-1.fsx
+++ pdf11-10/10-1.fsx
@@ -13,18 +13,68 @@ let show len dis =
     printfn "%04x: %-14s  %s" pc (String.concat " " words) dis
     pc <- pc + len
 
-let one (a:byte[]) b =
-    (read8 a b).[0..1]
-let two (a:byte[]) b =
-    (read8 a b).[2..3]
-let three (a:byte[]) b =
-    (read8 a b).[4..5]
+let operation10 (a:byte[]) b =
+    match (read8 a b).[2..3] with
+    | "00" ->
+        sprintf "bpl"
+    | "04" ->
+        sprintf "bmi"
+    | "10" ->
+        sprintf "bhi"
+    | "44" | "45" | "46" | "47" ->
+        sprintf "trap"
+
+    | _ ->
+        sprintf "??"
+
+let operation (a:byte[]) b =
+    match (read8 a b).[0..1] with
+    | "01" ->
+        sprintf "mov"
+    | "10" ->
+        operation10 a b
+    | "11" ->
+        sprintf "movb"
+    | "16" ->
+        sprintf "sub"
+    | _ ->
+        sprintf "??"
+
+let mode7 (a:byte[]) b c =
+    match (read8 a b).[c] with
+    | '2' ->
+        sprintf "#(%c)" (read8 a b).[c]
+    | '3' ->
+        sprintf "@#(%c)" (read8 a b).[c]
+    | '6' ->
+        sprintf "%c" (read8 a b).[c]
+    | '7' ->
+        sprintf "%c" (read8 a b).[c]
+
+let mode (a:byte[]) b c =
+    if (read8 a b).[c + 1] = '7' then
+        mode7 a b c
+    else
+        match (read8 a b).[c] with
+        | '0' ->
+            sprintf "r%c" (read8 a b).[c + 1]
+        | '1' ->
+            sprintf "(r%c)" (read8 a b).[c + 1]
+        | '2' ->
+            sprintf "(r%c)+" (read8 a b).[c + 1]
+        | '3' ->
+            sprintf "@(r%c)+" (read8 a b).[c + 1]
+        | '4' ->
+            sprintf "-(r%c)" (read8 a b).[c + 1]
+        | '5' ->
+            sprintf "@-(r%c)" (read8 a b).[c + 1]
+        | '6' ->
+            sprintf "X(r%c)" (read8 a b).[c + 1]
+        | '7' ->
+            sprintf "@X(r%c)" (read8 a b).[c + 1]
 
 while pc < tsize do
-    printfn "%s" (read8 mem pc)
-    printfn "%s" (one mem pc)
-    printfn "%s" (two mem pc)
-    printfn "%s" (three mem pc)
+    printfn "%s %s, %s" (operation mem pc) (mode mem pc 2) (mode mem pc 4)
     match read16 mem pc with
     | 0x1009 ->
         show 2 "mov r0, (r1)"
