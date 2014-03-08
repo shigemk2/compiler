4e074e10529fef0476a785a239ec62c119f6e350
shigemk2 <i.am.shige@gmail.com>
2014-03-08 17:17:57 +0900

    Apply mov movb sub

--- pdp11-10/10-1.fsx
+++ pdp11-10/10-1.fsx
@@ -11,6 +11,11 @@ let show oldpc dis =
     let words = [ for i in oldpc .. 2 .. pc - 1 -> sprintf "%04x" (read16 mem i) ]
     printfn "%04x: %-14s  %s" oldpc (String.concat " " words) dis
 
+// 　　|95c9|←instr
+// PC→|0048|←operand n 引数
+// 　　|15c0|
+// 012731 さらに先のアドレスを読むかどうかを決める
+
 // fetch 中で変数の値を書き換えているので副作用が発生する
 let fetch() =
     let ret = read16 mem pc
@@ -18,41 +23,39 @@ let fetch() =
     ret
 // operand取得 t type r register
 let getopr t r =
-    match t with
-    | 0 -> sprintf "r%d" r
-    | 1 -> sprintf "(r%d)" r
-    | 6 -> let opr = fetch()
-           sprintf "%04x" (pc + opr)
-    | _ -> "??"
+    if r = 7 then
+        match t with
+        | 2 -> sprintf "$%x" (fetch())
+        | 6 -> let opr = fetch()
+               sprintf "%04x" (pc + opr)
+        | _ -> "??"
+    else
+        match t with
+        | 0 -> sprintf "r%d" r
+        | 1 -> sprintf "(r%d)" r
+        | 2 -> sprintf "(r%d)+" r
+        | 6 -> sprintf "%x(r%d)" (fetch()) r
+        | _ -> "??"
 
 while pc < tsize do
     let oldpc = pc
     match fetch() with
-    | 0o010011 ->
-        show oldpc "mov r0, (r1)"
-    | 0o010261 ->
-        show oldpc (sprintf "mov r2, %x(r1)" (fetch()))
-    // | 0o012767 ->
-    //     // let opr1, opr2 = fetch(), fetch() このような書き方だと環境依存で実行順序が保証されない
-    //     let opr1 = fetch()
-    //     let opr2 = fetch() // (pc + 6 + read16 mem (pc + 4)) fetchが全部済んだ段階のプログラムカウンター
-    //     // 中で値をとったら進む、を繰り返す=fetch(CPU用語)
-    //     show oldpc (sprintf "mov $%x, %04x" opr1 (pc + opr2))
+    // let opr1, opr2 = fetch(), fetch() このような書き方だと環境依存で実行順序が保証されない
+    // (pc + 6 + read16 mem (pc + 4)) fetchが全部済んだ段階のプログラムカウンター
+    // 中で値をとったら進む、を繰り返す=fetch(CPU用語)
     // // switch-caseの高級なやつで、式が書ける(パターンマッチ)
-    | w when (w &&& 0o177770) = 0o012700 || w = 0o012767 ->
-        let opr1 = fetch()
-        let t, r = (w >>> 3 &&& 7), w &&& 7
-        show oldpc (sprintf "mov $%x, %s" opr1 (getopr t r))
-    | 0o112761 ->
-        let opr1 = fetch()
-        let opr2 = fetch()
-        show oldpc (sprintf "movb $%x, %x(r1)" opr1 opr2)
-    | 0o112711 ->
-        show oldpc (sprintf "movb $%x, (r1)" (fetch()))
-    | 0o110011 ->
-        show oldpc "movb r0, (r1)"
-    | 0o110061 ->
-        show oldpc (sprintf "movb r0, %x(r1)" (fetch()))
+    | w when (w >>> 12  = 0o01) ->
+        let t1, r1 = (w >>> 9 &&& 7), (w >>> 6 &&& 7)
+        let t2, r2 = (w >>> 3 &&& 7), (w       &&& 7)
+        show oldpc (sprintf "mov %s, %s" (getopr t1 r1) (getopr t2 r2))
+    | w when (w >>> 12  = 0o11) ->
+        let t1, r1 = (w >>> 9 &&& 7), (w >>> 6 &&& 7)
+        let t2, r2 = (w >>> 3 &&& 7), (w       &&& 7)
+        show oldpc (sprintf "movb %s, %s" (getopr t1 r1) (getopr t2 r2))
+    | w when (w >>> 12  = 0o16) ->
+        let t1, r1 = (w >>> 9 &&& 7), (w >>> 6 &&& 7)
+        let t2, r2 = (w >>> 3 &&& 7), (w       &&& 7)
+        show oldpc (sprintf "sub %s, %s" (getopr t1 r1) (getopr t2 r2))
     | 0o000300 ->
         show oldpc "swab r0"
     | 0o104401 ->
@@ -71,15 +74,5 @@ while pc < tsize do
         let oldpc = pc
         fetch() |> ignore
         show oldpc "; arg"
-    | 0o112767 ->
-        // 中で値をとったら進む、を繰り返す=fetch(CPU用語)
-        let opr1 = fetch()
-        let opr2 = fetch()
-        show oldpc (sprintf "movb $%x, %04x" opr1 opr2)
-    | 0o162767 ->
-        // 中で値をとったら進む、を繰り返す=fetch(CPU用語)
-        let opr1 = fetch()
-        let opr2 = fetch()
-        show oldpc (sprintf "sub $%x, %04x" opr1 opr2)
     | _ ->
         show oldpc "???"
