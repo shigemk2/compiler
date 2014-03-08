d95ed48ba7680c332cf3bb00e998d016707e21bd
shigemk2 <i.am.shige@gmail.com>
2014-03-08 15:00:51 +0900

    Fix oldpc

--- pdf11-10/10-1.fsx
+++ pdf11-10/10-1.fsx
@@ -9,7 +9,7 @@ let show oldpc len dis =
     let words = [ for i in oldpc .. 2 .. oldpc + len - 1 -> sprintf "%04x" (read16 mem i) ]
     printfn "%04x: %-14s  %s" oldpc (String.concat " " words) dis
     pc <- oldpc + len
-// fetch
+// fetch 中で変数の値を書き換えているので副作用が発生する
 let fetch() =
     let ret = read16 mem pc
     pc <- pc + 2
@@ -27,22 +27,26 @@ while pc < tsize do
     | 0o010011 ->
         show oldpc 2 "mov r0, (r1)"
     | 0o010261 ->
-        show oldpc 4 (sprintf "mov r2, %x(r1)" (read16 mem (pc + 2)))
+        show oldpc 4 (sprintf "mov r2, %x(r1)" (fetch()))
     | 0o012767 ->
-        show oldpc 6 (sprintf "mov $%x, %04x" (read16 mem (pc + 2)) (pc + 6 + read16 mem (pc + 4)))
+        // let opr1, opr2 = fetch(), fetch() このような書き方だと環境依存で実行順序が保証されない
+        let opr1 = fetch()
+        let opr2 = fetch() // (pc + 6 + read16 mem (pc + 4)) fetchが全部済んだ段階のプログラムカウンター
+        // 中で値をとったら進む、を繰り返す=fetch(CPU用語)
+        show oldpc 6 (sprintf "mov $%x, %04x" opr1 (pc + opr2))
     // switch-caseの高級なやつで、式が書ける(パターンマッチ)
     | w when (w &&& 0o177770) = 0o012700 ->
-        let w2 = read16 mem (pc + 2)
+        // let w2 = read16 mem (pc + 2)
         let t, r = (w >>> 3 &&& 7), w &&& 7
-        show oldpc 4 (sprintf "mov $%x, %s" w2 (getopr t r))
+        show oldpc 4 (sprintf "mov $%x, %s" (fetch()) (getopr t r))
     | 0o112761 ->
-        show oldpc 6 (sprintf "movb $%x, %x(r1)" (read16 mem (pc + 2)) (read16 mem (pc + 4)))
+        show oldpc 6 (sprintf "movb $%x, %x(r1)" (fetch()) (read16 mem (pc + 4)))
     | 0o112711 ->
-        show oldpc 4 (sprintf "movb $%x, (r1)" (read16 mem (pc + 2)))
+        show oldpc 4 (sprintf "movb $%x, (r1)" (fetch()))
     | 0o110011 ->
         show oldpc 2 "movb r0, (r1)"
     | 0o110061 ->
-        show oldpc 4 (sprintf "movb r0, %x(r1)" (read16 mem (pc + 2)))
+        show oldpc 4 (sprintf "movb r0, %x(r1)" (fetch()))
     | 0o000300 ->
         show oldpc 2 "swab r0"
     | 0o104401 ->
@@ -52,8 +56,8 @@ while pc < tsize do
         show pc 2 "; arg"
         show pc 2 "; arg"
     | 0o112767 ->
-        show oldpc 6 (sprintf "movb $%x, %04x" (read16 mem (pc + 2)) (pc + 6 + read16 mem (pc + 4)))
+        show oldpc 6 (sprintf "movb $%x, %04x" (fetch()) (pc + 6 + read16 mem (pc + 4)))
     | 0o162767 ->
-        show oldpc 6 (sprintf "sub $%x, %04x" (read16 mem (pc + 2)) (pc + 6 + read16 mem (pc + 4)))
+        show oldpc 6 (sprintf "sub $%x, %04x" (fetch()) (pc + 6 + read16 mem (pc + 4)))
     | _ ->
         show oldpc 2 "???"
