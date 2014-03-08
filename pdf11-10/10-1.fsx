961c815e45528f0ffa0bd418d19813bd5de61381
shigemk2 <i.am.shige@gmail.com>
2014-03-08 14:04:05 +0900

    Fix file path

--- pdf11-10/10-1.fsx
+++ pdf11-10/10-1.fsx
@@ -1,4 +1,4 @@
-let aout = System.IO.File.ReadAllBytes "write-7.out"
+let aout = System.IO.File.ReadAllBytes "write-8.out"
 let read16 (a:byte[]) b =
     (int a.[b]) ||| ((int a.[b+1]) <<< 8)
 let tsize = read16 aout 2
