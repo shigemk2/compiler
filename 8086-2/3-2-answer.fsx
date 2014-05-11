02be973cb8fb27dc1c26051b0d85d590a0155568
shigemk2 <i.am.shige@gmail.com>
2014-05-11 16:03:46 +0900

    8086 数値をメモリに書き込む インタプリタ https://bitbucket.org/7shi/ikebin/wiki/8086/write-1

---
+++ 8086-2/3-2-answer.fsx
@@ -0,0 +1,41 @@
+let aout = System.IO.File.ReadAllBytes "write-1.out"
+let read16 (a:byte[]) b =
+    (int a.[b]) ||| ((int a.[b + 1]) <<< 8)
+let write16 (a:byte[]) b c =
+    a.[b] <- byte c
+    a.[b + 1] <- byte (c >>> 8)
+let tsize = read16 aout 2
+let dsize = read16 aout 4
+let mem = Array.zeroCreate<byte> 0x10000
+mem.[0 .. tsize + dsize - 1] <- aout.[16 .. 16 + tsize + dsize - 1]
+let mutable ax, bx, ip = 0, 0, 0
+while ip < tsize do
+    match int mem.[ip], int mem.[ip + 1] with
+    | 0xb8, _ ->
+        ax <- read16 mem (ip + 1)
+        ip <- ip + 3
+    | 0xbb, _ ->
+        bx <- read16 mem (ip + 1)
+        ip <- ip + 3
+    | 0xc7, 0x07 ->
+        write16 mem bx (read16 mem (ip + 2))
+        ip <- ip + 4
+    | 0xcd, 0x07 ->
+        match int mem.[ip + 2] with
+        | 1 ->
+            exit ax
+        | 4 ->
+            let arg1 = read16 mem (ip + 3)
+            let arg2 = read16 mem (ip + 5)
+            let bytes = mem.[arg1 .. arg1 + arg2 - 1]
+            printf "%s" (System.Text.Encoding.ASCII.GetString bytes)
+            ip <- ip + 7
+        | sc ->
+            printfn "%04x: ??? syscall %d" ip sc
+            exit 1
+    | 0xcd, n ->
+        printfn "%04x: ??? int %x" ip n
+        exit 1
+    | b, _ ->
+        printfn "%04x: %02x ???" ip b
+        exit 1
---
+++ 8086-2/3-2.fsx
@@ -0,0 +1,41 @@
+let aout = System.IO.File.ReadAllBytes "write-1.out"
+let read16 (a:byte[]) b =
+    (int a.[b]) ||| ((int a.[b + 1]) <<< 8)
+let write16 (a:byte[]) b c =
+  a.[b] <- byte c
+  a.[b + 1] <- byte (c >>> 8)
+let tsize = read16 aout 2
+let dsize = read16 aout 4
+let mem = aout.[16 .. 16 + tsize + dsize - 1]
+let mutable ax, bx, ip = 0, 0, 0
+while ip < tsize do
+    match int mem.[ip] with
+    | 0xb8 ->
+        ax <- read16 mem (ip + 1)
+        ip <- ip + 3
+    | 0xbb ->
+        bx <- read16 mem (ip + 1)
+        ip <- ip + 3
+    | 0xc7 ->
+        write16 mem bx (read16 mem (ip + 2))
+        ip <- ip + 4
+    | 0xcd ->
+        let n = int mem.[ip + 1]
+        if n <> 7 then
+            printfn "%04x: ??? int %x" ip n
+            exit 1
+        match int mem.[ip + 2] with
+        | 1 ->
+            exit ax
+        | 4 ->
+            let arg1 = read16 mem (ip + 3)
+            let arg2 = read16 mem (ip + 5)
+            let bytes = mem.[arg1 .. arg1 + arg2 - 1]
+            printf "%s" (System.Text.Encoding.ASCII.GetString bytes)
+            ip <- ip + 7
+        | sc ->
+            printfn "%04x: ??? syscall %d" ip sc
+            exit 1
+    | b ->
+        printfn "%04x: %02x ???" ip b
+        exit 1
