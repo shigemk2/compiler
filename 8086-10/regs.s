dfa561ba086fa9491246499d7eb28b502268e6a8
shigemk2 <i.am.shige@gmail.com>
2014-06-22 15:10:17 +0900

    Fix assemble

--- 8086-10/regs.s
+++ 8086-10/regs.s
@@ -10,6 +10,19 @@ mov (bx), ax
 mov (bx), cx
 mov (bx), dx
 mov (bx), bx
+mov (bx), sp
+mov (bx), bp
+mov (bx), si
+mov (bx), di
+
+mov 2(bx), ax
+mov 2(bx), cx
+mov 2(bx), dx
+mov 2(bx), bx
+mov 2(bx), sp
+mov 2(bx), bp
+mov 2(bx), si
+mov 2(bx), di
 
 .sect .data
 hello: .ascii "hello\n"
